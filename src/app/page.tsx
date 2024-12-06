50172311e57a4a281b6d37e8602c36fdd272a4b4
DevSolo-Tech <2021bse109@std.must.ac.ug>
2024-12-06 23:21:16 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", flexDirection: "column" }}>
-      <h1>Welcome to My First Next.js App!</h1>
-      <p>Powered by React, Next.js, and Tailwind CSS 🚀</p>
+      <h1>Welcome to Our First Hosted Next.js App!</h1>
+      <p>Prowered by brilliant minds</p>
     </main>
   );
 }
